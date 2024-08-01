d121260ac0eaf5baa07dfa1376d836cee580601b
BUZZ <113298285+BDTA-zky@users.noreply.github.com>
2024-08-01 19:49:17 +0800

    更改思维导图icon

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Atom, PaintBrush, Python , KissWinkHeart, Heart, Search, Sitemap， MapRegular} from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Atom, PaintBrush, Python , KissWinkHeart, Heart, Search, Sitema, MapRegular} from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -76,7 +76,7 @@ const siteIcon = {
   KissWinkHeart,
   Heart,
   Search,
-  Sitemap，
+  Sitemap,
   MapRegular
 };
 
