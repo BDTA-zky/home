c387e17bdf80d31f86c5cb0a5a3a2e45fd982e32
BUZZ <113298285+BDTA-zky@users.noreply.github.com>
2024-08-05 20:21:41 +0800

    增加docs网站

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Atom, PaintBrush, Python , KissWinkHeart, Heart, Search, MapRegular } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Atom, PaintBrush, Python , KissWinkHeart, Heart, Search, MapRegular, BookOpen } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -76,7 +76,8 @@ const siteIcon = {
   KissWinkHeart,
   Heart,
   Search,
-  MapRegular
+  MapRegular,
+  BookOpen
 };
 
 // 链接跳转
